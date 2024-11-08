e51b6f1c87864f77fd797c645b720bdfe450f765
tabithasim <tabitha.sim.2023@scis.smu.edu.sg>
2024-11-08 16:50:26 +0800

    friends navbar

--- frontend/src/views/Protected/Friends.vue
+++ frontend/src/views/Protected/Friends.vue
@@ -242,24 +242,20 @@ onMounted(() => {
   width: 250px;
   height: 100vh;
   position: fixed;
-  top: 0;
-  left: 0;
-  background-color: #ffffff;
-  z-index: 1;
 }
 
 main {
   align-items: center;
-  /* Center horizontally */
   margin-left: 250px;
   flex-grow: 1;
-  padding: 5%;
   display: flex;
   flex-direction: column;
   gap: 5px;
   background-color: #FCEFB4;
   height: 100vh;
   overflow: hidden;
+  padding: 20px;
+  box-sizing: border-box;
 }
 
 #scrollable-element {
@@ -274,5 +270,58 @@ main {
   font-weight: bold;
   margin-bottom: 10px;
   text-align: center;
+}
+
+
+/* Responsive Adjustments */
+@media (max-width: 991px) {
+  .navbar {
+    width: 80px;
+  }
+
+  main {
+    margin-left: 80px;
+    padding: 15px;
+  }
+
+  .floating-btn {
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+  }
+}
+
+@media (max-width: 767px) {
+  .home-container {
+    flex-direction: column;
+  }
+
+  .navbar {
+    width: 100%;
+    height: 50px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background-color: #f9f9f9;
+    z-index: 10;
+  }
+
+  main {
+    margin-left: 0;
+    margin-top: 0;
+    padding: 15px;
+    height: calc(100vh - 50px);
+    /* Account for navbar height on mobile */
+    overflow-y: auto;
+  }
+
+  .floating-btn {
+    bottom: 60px;
+    /* Place above mobile navbar */
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    font-size: 30px;
+  }
 }
 </style>
